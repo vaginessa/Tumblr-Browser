e14c3b0414531bda867ef955177cc2fea0a7da58
MaciekBro <maciekbrodowski@gmail.com>
2017-03-10 19:15:38 +0100

    Init project commit

---
+++ app/src/main/java/com/example/maciek/tumblrbrowser/SearchActivity.java
@@ -0,0 +1,25 @@
+package com.example.maciek.tumblrbrowser;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SearchActivity.this,ScrollingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
