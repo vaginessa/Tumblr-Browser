5094ab79a4b08cf45c80a784adc903fe346f6546
MaciekBro <maciekbrodowski@gmail.com>
2017-03-13 16:44:02 +0100

    change in PostDetail

--- app/src/main/java/com/example/maciek/tumblrbrowser/PostDetail.java
+++ app/src/main/java/com/example/maciek/tumblrbrowser/PostDetail.java
@@ -1,8 +1,17 @@
 package com.example.maciek.tumblrbrowser;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Maciek on 2017-03-13.
  */
 public class PostDetail {
+
+    String url;
+    String photo;
+    String date;
+
+//    @SerializedName(photo-)
+//    String photoURL;
 
 }
