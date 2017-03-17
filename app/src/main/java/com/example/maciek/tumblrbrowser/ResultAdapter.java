e69b6993ec30bc6d49927ff0e71d89a7f60b8a40
MaciekBro <maciekbrodowski@gmail.com>
2017-03-17 01:55:25 +0100

    added vodeo post layout

--- app/src/main/java/com/example/maciek/tumblrbrowser/ResultAdapter.java
+++ app/src/main/java/com/example/maciek/tumblrbrowser/ResultAdapter.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 public class ResultAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    private static final int VIDEO_POST = 3;
     private List<PostDetail> postList = Collections.emptyList();
     private OnItemClickActionListener onItemClickActionListener;
     private static final int PHOTO_POST = 1;
@@ -27,7 +28,6 @@ public class ResultAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     public void setOnItemClickActionListener(OnItemClickActionListener onItemClickActionListener) {
         this.onItemClickActionListener = onItemClickActionListener;
     }
-
 
 
     public void setPostList(List<PostDetail> postList) {
@@ -44,6 +44,9 @@ public class ResultAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         if (viewType == PHOTO_POST) {
             View layoutPostsLists = LayoutInflater.from(parent.getContext()).inflate(R.layout.post_photo, parent, false);
             return new PhotoViewHolder(layoutPostsLists);
+        } else if (viewType == VIDEO_POST) {
+            View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.post_video, parent, false);
+            return new VideoViewHolder(layout);
         } else {
             View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.post_text, parent, false);
             return new TextViewHolder(layout);
@@ -56,6 +59,7 @@ public class ResultAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             TextViewHolder textViewHolder = (TextViewHolder) holder;
             textViewHolder.slugPostText.setText(postList.get(position).getSlug().replace("-", " ") + "...");
             textViewHolder.dateTextPost.setText("Poseted on: " + postList.get(position).getDate());
+            textViewHolder.typeTextPost.setText("Type: " + postList.get(position).getType());
             textViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -64,13 +68,29 @@ public class ResultAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
                     }
                 }
             });
+        } else if (getItemViewType(position) == VIDEO_POST) {
+            VideoViewHolder videoViewHolder = (VideoViewHolder) holder;
+            videoViewHolder.dateVideoPost.setText("Poseted on: " + postList.get(position).getDate());
+            videoViewHolder.slugVideoPost.setText(postList.get(position).getDate());
+            videoViewHolder.typeVideoPost.setText("Type: " + postList.get(position).getType());
+            videoViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (onItemClickActionListener != null) {
+                        onItemClickActionListener.onItemClick(postList.get(position).getUrl().replace("\\", ""));
+                    }
+                }
+            });
+
         } else {
             PhotoViewHolder photoViewHolder = (PhotoViewHolder) holder;
             String url = postList.get(position).getPhotoUrl();
+            //TODO zfixować replace
             url = url.replace("\\", "");
             Glide.with(photoViewHolder.imagePhotoPost.getContext()).load(url).into(photoViewHolder.imagePhotoPost);
             photoViewHolder.slugPhotoPost.setText(postList.get(position).getSlug().replace("-", " ") + "...");
             photoViewHolder.datePhotoPost.setText("Poseted on: " + postList.get(position).getDate());
+            photoViewHolder.typePhotoPost.setText("Type: " + postList.get(position).getType());
             photoViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -86,6 +106,8 @@ public class ResultAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     public int getItemViewType(int position) {
         if (postList.get(position).getType().equals("regular")) {
             return TEXT_POST;
+        } else if (postList.get(position).getType().equals("video")) {
+            return VIDEO_POST;
         } else {
             return PHOTO_POST;
         }
@@ -102,12 +124,14 @@ public class ResultAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         ImageView imagePhotoPost;
         TextView slugPhotoPost;
         TextView datePhotoPost;
+        TextView typePhotoPost;
 
         public PhotoViewHolder(View itemView) {
             super(itemView);
             imagePhotoPost = (ImageView) itemView.findViewById(R.id.photo_post_image);
             slugPhotoPost = (TextView) itemView.findViewById(R.id.photo_post_slug);
             datePhotoPost = (TextView) itemView.findViewById(R.id.photo_post_date);
+            typePhotoPost = (TextView) itemView.findViewById(R.id.photo_post_type);
         }
 
     }
@@ -116,11 +140,28 @@ public class ResultAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
         TextView slugPostText;
         TextView dateTextPost;
+        TextView typeTextPost;
 
         public TextViewHolder(View itemView) {
             super(itemView);
             slugPostText = (TextView) itemView.findViewById(R.id.text_post_slug);
             dateTextPost = (TextView) itemView.findViewById(R.id.text_post_date);
+            typeTextPost = (TextView) itemView.findViewById(R.id.text_post_type);
         }
     }
+
+    class VideoViewHolder extends RecyclerView.ViewHolder {
+
+        TextView slugVideoPost;
+        TextView dateVideoPost;
+        TextView typeVideoPost;
+
+        public VideoViewHolder(View itemView) {
+            super(itemView);
+            slugVideoPost = (TextView) itemView.findViewById(R.id.video_post_slug);
+            dateVideoPost = (TextView) itemView.findViewById(R.id.video_post_date);
+            typeVideoPost = (TextView) itemView.findViewById(R.id.video_post_type);
+
+        }}
+
 }
